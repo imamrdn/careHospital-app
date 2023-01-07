8ec651e58071e15bad80db84d2f5604f03a9267e
Imam Ramadhan <imamrmdn99@gmail.com>
2023-01-07 08:19:39 +0700

    update: LoginActivity

--- app/src/main/java/com/example/carehospital_app/LoginActivity.java
+++ app/src/main/java/com/example/carehospital_app/LoginActivity.java
@@ -1,14 +1,24 @@
 package com.example.carehospital_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText editEmail, editPassword;
     Button buttonLogin;
+    SharedPreferences sharedPreferences;
+
+    private static final String SHARED_PREF_NAME = "mypref";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_PASSWORD = "password";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +29,28 @@ public class LoginActivity extends AppCompatActivity {
         editPassword = findViewById(R.id.editPassword);
         buttonLogin = findViewById(R.id.buttonLogin);
 
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
 
+        String email = sharedPreferences.getString(KEY_EMAIL, null);
+        if (email != null) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(KEY_EMAIL,editEmail.getText().toString());
+                editor.putString(KEY_PASSWORD, editPassword.getText().toString());
+                editor.apply();
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
